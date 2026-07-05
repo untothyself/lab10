a485b27efe9a40b2d41516e1090bb3667c35cda6
Abass Hassan <Ahassan124@student.cscc.edu>
2026-07-05 17:30:02 -0400

    Created file process

--- Lab10/Lab10_ahassan124-1.py
+++ Lab10/Lab10_ahassan124-1.py
@@ -24,3 +24,26 @@ class WordAnalyzer:
     def __init__(self, filepath):
         self.__filepath = Path(filepath)
         self.__frequencies = {}
+    def process_file(self):
+        try:
+            if not self.__filepath.exists():
+                raise FileNotFoundError
+
+            translator = str.maketrans('', '', string.punctuation)
+
+            with self.__filepath.open("r", encoding="utf-8") as file:
+                for line in file:
+                    line = line.lower()
+                    line = line.translate(translator)
+                    words = line.split()
+                    for word in words:
+                        if word in self.__frequencies:
+                            self.__frequencies[word] += 1
+                        else:
+                            self.__frequencies[word] = 1
+
+            return True
+
+        except FileNotFoundError:
+            print("Error: File not found.")
+            return False
